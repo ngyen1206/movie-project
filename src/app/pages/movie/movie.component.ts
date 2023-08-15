eb84ad78484a73c4875e1d3cb0fbc5ca0c70fc6b
ledat <dat.le@sstechvn.com>
2023-08-15 17:23:38 +0700

    edit page detail

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from 'src/app/service/data.service';
 
 @Component({
@@ -8,25 +9,55 @@ import { DataService } from 'src/app/service/data.service';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
-  public tilte = ['Overview', 'Popularity', 'Vote Average'];
+  public safeUrl: any;
   public array = [1, 2, 3, 4];
   @Input()
+  public poster_path: string = '';
+  @Input()
+  public original_title: string = '';
+  @Input()
   public movie: any = {};
+
+  @Input()
+  public movie1: any = {};
+
+  @Input()
+  public data: any = {};
+  public key_movie: any = '';
   constructor(
     private service: DataService,
     private router: ActivatedRoute,
+    private router1: Router,
+    private sanitizer: DomSanitizer
   ) {
-
+    router1.events.subscribe(data => {
+      console.log(data);
+      this.loadData();
+      // this.reloadPage();
+      // return;
+      
+    })
   }
-  showVideoMovie() {
-
+  reloadPage(){
+    window.location.reload();
+    window.scrollTo(0, 0);
+    return;
   }
+
   async ngOnInit(): Promise<void> {
-    const id = this.router.snapshot.paramMap.get('id');
+    this.loadData();
+    this.data = await this.service.getListMoviesKnowLength(5);
+  }
 
+  async loadData() {
+    const id = this.router.snapshot.paramMap.get('id');
     if (id) {
-      this.movie = await this.service.getMovie(id)
+      this.movie = await this.service.getMovie(id);
+      this.movie1 = await this.service.getVideoMovie(id);
+      this.key_movie = this.movie1.results[0].key;
+      const unsafeUrl = `https://www.youtube.com/embed/${this.key_movie}`;
+      this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl);
     };
   }
-
+ 
 }
--- src/app/pages/movie/movie.module.ts
+++ src/app/pages/movie/movie.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieComponent } from './movie.component';
 import { MovieRoutingModule } from './movie-routing.module';
-import { CardMovieSmallModule } from '../home/components/card-movie-small/card-movie-small.module';
-import { CardMovieSmallComponent } from '../home/components/card-movie-small/card-movie-small.component';
 import { CommentComponent } from './comment/comment.component';
+import { CardMovieSmallComponent } from '../home/components/card-movie-small/card-movie-small.component';
+import { CardMovieSmallModule } from '../home/components/card-movie-small/card-movie-small.module';
+import { CommentModule } from './comment/comment.module';
 
 
 
 @NgModule({
   declarations: [
     MovieComponent,
-    CardMovieSmallComponent,
-    CommentComponent
   ],
   imports: [
     CommonModule,
     MovieRoutingModule,
-    //CardMovieSmallModule
+    CardMovieSmallModule,
+    CommentModule
   ]
 })
 export class MovieModule { }
